725bd1e4ca66560bfc4689759b47dde254936a37
Guadalupe Fernanda Garcia Cortes <g.fernanda_gc20@outlook.com>
2022-07-27 12:49:06 -0500

    init

---
+++ src/main/java/com/mycompany/programa_n/Programa_n.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.programa_n;
+
+
+
+
+public class Programa_n {
+
+    public static void main(String[] args) {
+        double i= 4;
+        for(i=1;i<=4;i++){
+        double elevado = Math.pow(i,2);   
+            System.out.println("el numero elevado es: " + elevado);
+            
+        }
+        
+    
+}
+}
